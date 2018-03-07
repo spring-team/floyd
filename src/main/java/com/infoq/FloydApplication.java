602b76629da328e16de577eca9084d0f9594bb80
Rod Johnson <johnsonroda@gmail.com>
2018-03-07 12:00:01 +0000

    Everyone needs encouragement to write Java

--- src/main/java/com/infoq/FloydApplication.java
+++ src/main/java/com/infoq/FloydApplication.java
@@ -1,5 +1,7 @@
 // It’s only a thought, and a thought can be changed.
 
+// It’s only a thought, and a thought can be changed.
+
 // I believe in you. You can do the thing!
 
 // I come from the loving space of my heart, and I know that love opens all doors.
