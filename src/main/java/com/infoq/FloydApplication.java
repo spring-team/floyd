e9b1b6bc0d191309ecb37c53ce7eb715549c230e
Rod Johnson <johnsonroda@gmail.com>
2018-03-07 11:10:59 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/infoq/FloydApplication.java
@@ -0,0 +1,12 @@
+package com.infoq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloydApplication.class, args);
+	}
+}
---
+++ src/test/java/com/infoq/FloydApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infoq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FloydApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
